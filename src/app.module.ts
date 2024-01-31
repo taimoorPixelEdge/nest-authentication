4b03313da31903623c154e458cbc9e990d7a0334
taimoorPixelEdge <muhammad.taimoor@pixeledge.io>
2024-02-01 00:07:54 +0500

    Initail Commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import {JwtModule} from "@nestjs/jwt";
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'easygenerator_assesment',
+    entities: [User],
+    synchronize: true,
+  }), TypeOrmModule.forFeature([User]),JwtModule.register({
+    secret: 'secret',
+    signOptions: {expiresIn: '1d'}
+})],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
